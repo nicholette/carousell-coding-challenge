b4059ae0053b326e708688d7f778c8b5ae6d7b79
Nicholette <nekonekonik@gmail.com>
2018-02-23 16:32:00 +0800

    Add enzyme, mock test

---
+++ app/test/topics.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import createReactClass from 'create-react-class';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+
+const Test = createReactClass({
+  render() {
+    return (
+      <View>
+        <Text>enzyme</Text>
+        <Text>rules</Text>
+      </View>
+    )
+  }
+});
+describe('<Topics />', () => {
+  it('it should render 1 view component', () => {
+    const wrapper = shallow(<Test/>);
+    expect(wrapper.find(View)).to.have.length(1);
+  });
+
+  it('it should render 2 text components', () => {
+    const wrapper = shallow(<Test/>);
+    expect(wrapper.find(Text)).to.have.length(2);
+  });
+});
